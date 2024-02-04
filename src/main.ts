4e2f8d67451ae616bc7581bbd137c6d02daab230
kofta999 <mostafaxxx555@gmail.com>
2024-02-04 23:52:08 +0200

    Add TransformInterceptor to global interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpErrorFilter } from './filters/http-error.filter';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpErrorFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
 bootstrap();
